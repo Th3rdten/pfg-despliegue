5dfd986f75976e81c891e2cf067808e90a16ca3a
Th3rdten <traxhmo0n@gmail.com>
2026-01-18 18:40:03 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,15 +1,12 @@
-JavaScript
 const express = requive('express');
 const app = express();
-// Render nos dice en qué puerto escuchar a través de esta variable.
 const port = process.env.PORT || 3000; 
 
-// Cuando alguien entra a la página principal, envía un saludo.
 app.get('/', (req, res) => {
   res.send('¡Hola! Esta es mi primera aplicación desplegada en Render.');
 });
 
-// Le decimos a la aplicación que empiece a "escuchar" en el puerto.
 app.listen(port, () => {
   console.log(`Aplicación Node.js lista en el puerto ${port}`);
 });
+
